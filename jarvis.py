70a9de3187037e5fc6f35a27d8bc5a7f0de8d4ad
DaanVoTech <daaanvo.tech@gmail.com>
2021-03-04 06:09:59 +0100

    updated code

--- jarvis.py
+++ jarvis.py
@@ -62,13 +62,37 @@ if 'wikipedia' in query.lower():
     speak(results)
 
 elif 'open google' in query.lower():
-    speak("Opening google")
+    speak("Opening Google")
     url = "google.com"
     chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
     webbrowser.get(chrome_path).open(url)
 
 elif 'open youtube' in query.lower():
-    speak("Opening youtube")
+    speak("Opening Youtube")
     url = "youtube.com"
     chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
     webbrowser.get(chrome_path).open(url)
+
+elif 'open the weather' in query.lower():
+    speak("Opening the weather")
+    url = "https://www.google.com/search?q=weather&oq=weather&aqs=chrome..69i57j0j0i131i433l2j0l2j46j46i131i433j0j0i131i433.1564j0j7&sourceid=chrome&ie=UTF-8"
+    chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
+    webbrowser.get(chrome_path).open(url)
+
+elif 'open the news' in query.lower():
+    speak("Opening the news")
+    url = "https://news.google.com/topstories?hl=nl&gl=NL&ceid=NL:nl"
+    chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
+    webbrowser.get(chrome_path).open(url)
+
+elif 'play music' in query.lower():
+    speak("Opening the music")
+    songs_dir = "music"
+    songs = os.listdir(songs_dir)
+    print(songs)
+    os.startfile(os.path.join(songs_dir, songs[0]))
+
+elif 'the time' in query.lower():
+    strTime = datetime.datetime.now().strftime("%H:%M:%S")
+    print(strTime)
+    speak(f"{MASTER} the time is {strTime}")
